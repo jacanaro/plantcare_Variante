d6d0c274379d5111545724bf08fe8d526a315437
jacanaro <aaron_jacob4692@yahoo.de>
2022-01-21 12:54:26 +0100

    3d-models, pflanzenprofil
    modelle umbenannt für db und mangageModel script angepasst

--- PlantCare 2.0/Assets/Scripts/managePlantModel.cs
+++ PlantCare 2.0/Assets/Scripts/managePlantModel.cs
@@ -1,31 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class managePlantModel : MonoBehaviour
 {
-    public GameObject modelWraper;
-    public GameObject[] modelTextures = new GameObject[3];
-    public Camera cam;
+    [SerializeField] private GameObject modelWraper;
+    [SerializeField] private GameObject[] modelTextures = new GameObject[12];
+    private string dbName = "URI=file:Plants.db";
 
     void Start()
     {
+        string plantname=PlayerPrefs.GetString("name");
+        int plantID= PlayerPrefs.GetInt("plantID");
+        Debug.Log(plantID);
+                //create the db connection
+        using (var connection = new SqliteConnection(dbName)) {
+            connection.Open();
+
+            // set up an object (called "command") to allow db control
+            using (var command = connection.CreateCommand()) {
+                
+                //get generalInfo
+                command.CommandText = "SELECT plantStage FROM userPlants, publicPlants where userPlants.latName=publicPlants.latName AND plantID='"+plantID+"';";
+                
+                using (IDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        Debug.Log(reader["plantStage"]);
+                    }
+                    reader.Close();
+                }
+            }
+
+            connection.Close();
+        }
+
         foreach (GameObject x in modelTextures)
         {
             if(x.name.Equals("Paprikamittel")){
                 GameObject model = Instantiate(x);
-                model.transform.parent=modelWraper.transform;
-                model.transform.localPosition = new Vector3(-0.004f, -0.111f,-0.058f);
-                model.transform.rotation= Quaternion.Euler(0f, 1.849f, 0f);
-                model.transform.localScale= new Vector3(0.0387501f, 0.0387501f, 0.0387501f);
-                Debug.Log(model.transform.position);
-                Debug.Log(model.transform.rotation);
-                Debug.Log(model.transform.localScale);
-                Thread.Sleep(1000);
-                cam.cullingMask=0;
-                Thread.Sleep(1000);
-                cam.cullingMask=-1;
+                model.transform.position = new Vector3(0f,0.6f,-9f);
+                model.transform.localScale= new Vector3(0.1f, 0.1f, 0.1f);
             }
         }
     }
